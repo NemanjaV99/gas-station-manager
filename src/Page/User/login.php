5b508716c77afed3ebf06846c6a8eeca5ac96465
Nemanja <nemanjavukovic.work@gmail.com>
2020-01-04 18:48:39 +0100

    Add the Register and Login User pages
    Creating the pages for User registration and login operations.

---
+++ src/Page/User/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login User</title>
+</head>
+<body>
+    
+    <form action="#" method="POST" class="form">
+        <label for="input-email">Email:</label>
+        <input type="text" name="email" class="form__input" id="input-email">
+        <label for="input-password">Password:</label>
+        <input type="password" name="password" class="form__input" id="input-password">
+        <input type="submit" name="login" value="Log In">
+    </form>
+
+</body>
+</html>
---
+++ src/Page/User/register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register User</title>
+</head>
+<body>
+    
+    <form action="#" method="POST" class="form">
+        <label for="input-name">Name:</label>
+        <input type="text" name="name" class="form__input" id="input-name">
+        <label for="input-lname">Last name:</label>
+        <input type="text" name="lname" class="form__input" id="input-lname">
+        <label for="input-email">Email:</label>
+        <input type="text" name="email" class="form__input" id="input-email">
+        <label for="input-password">Password:</label>
+        <input type="password" name="password" class="form__input form__input--pass" id="input-password">
+        <label for="input-gstation">Gas Station:</label>
+        <input type="text" name="gstation" class="form__input" id="input-gstation">
+        <input type="submit" name="register" value="Register">
+    </form>
+
+</body>
+</html>
